c11cdeb8fdc283773ab0856a12a0c3b42798cc1b
Philippe R Bretas <86687733+prbretas@users.noreply.github.com>
2022-06-08 08:37:56 -0300

    add projects JAVA

---
+++ Philippe/Projeto 1/src/OlaMundo.java
@@ -0,0 +1,30 @@
+
+public class OlaMundo {
+    //método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo ");
+        System.out.println("ao IOS!");
+    }  //fim do método main
+} //fim da classe OlaMundo
+
+
+/*
+Descrição narrativa:
+
+Multiplicação de dois números:
+- Passo 1: Receber os dois números que serão multiplicados
+- Passo 2: Multiplicar os números
+- Passo 3: Mostrar o resultado obtido na multiplicação
+*/
+
+
+/*
+ALGORITMO
+DECLARE N1, N2, M NÚMÉRICO
+ESCREVA  'Digite dois números'
+LEIA N1, N2
+M =  N1 * N2
+ESCREVA 'Multiplicação = ', M
+FIM DO ALGORITMO
+*/
+
---
+++ Philippe/Projeto4_Oper_Log/Projeto4_Oper_Log.java
@@ -0,0 +1,10 @@
+public class Projeto4_Oper_Log {
+    // método main inicia a execução do aplicativo
+    public static void main(String[] args)
+    {
+        System.out.println("Teste1: " + (true && false));
+        System.out.println("Teste2: " + (true || false));
+        System.out.println("Teste: " + (!true));
+
+    } // fim do método main
+}// fim da classe OperLog
